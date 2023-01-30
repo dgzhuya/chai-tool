ddaf2863b10e1b4d4e27ad2cd4bac50e2ffc76a9
dgzhuya <chenbiuxiu@gmail.com>
2023-01-30 23:32:29 +0800

    🐞 fix: 使用base64展示图片

--- src/adb/image.ts
+++ src/adb/image.ts
@@ -1,26 +1,26 @@
-import { execAdbCommandById, execAdbShellById } from '@/utils/command'
-import { getResourcePath } from '@/utils/system'
-import fs from 'fs'
+import os from 'os'
 import path from 'path'
+import { execAdbCommandById, execAdbShellById } from '@/utils/command'
+import fs from 'fs'
 
-let fileDirPath = ''
+const fileDirPath =
+	os.platform() === 'darwin' && process.env.NODE_ENV !== 'development'
+		? path.resolve(__dirname, '../../')
+		: path.resolve(process.cwd(), 'resources')
 
 export const getScreenImg = async (id: string, fileName = `screenshot_${new Date().getTime()}.png`) => {
-	if (!fileDirPath) fileDirPath = getResourcePath()
 	try {
 		await execAdbShellById(id, `/system/bin/screencap -p /sdcard/${fileName}`)
 		await execAdbCommandById(id, `pull /sdcard/${fileName} ${fileDirPath}/${fileName}`)
 		execAdbShellById(id, `rm /sdcard/${fileName}`)
-		deletePreFile(id)
-		localStorage.setItem(`${id}_img`, fileName)
-		return `atom:///${fileName}`
+		const filePath = `${fileDirPath}/${fileName}`
+		if (fs.existsSync(filePath)) {
+			const base64 = fs.readFileSync(filePath).toString('base64')
+			fs.rm(filePath, err => err && console.log(err.message))
+			return `data:image/png;base64,${base64}`
+		}
 	} catch (error) {
 		console.log('error: ', error)
 	}
 	return ''
-}
-
-const deletePreFile = (id: string) => {
-	const preFile = localStorage.getItem(`${id}_img`)
-	if (preFile) fs.rm(path.resolve(fileDirPath, preFile), error => error && console.log(error.message))
 }
