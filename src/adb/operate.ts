4149c1d52a047c01837fa5a406dcd79ae5ae9f87
dgzhuya <chenbiuxiu@gmail.com>
2024-05-21 21:35:16 +0800

    ✨ feat: 使用滑动代替点击

--- src/adb/operate.ts
+++ src/adb/operate.ts
@@ -1,4 +1,8 @@
 import { execAdbShellById } from '@/utils/command'
 import { Point } from '@/types/tap'
 
-export const tapHandler = (id: string, postion: Point) => execAdbShellById(id, `input tap ${postion.x} ${postion.y}`)
+export const tapHandler = (id: string, postion: Point) => {
+	const endPosX = postion.x + Math.random() * 5
+	const endPosY = postion.y + Math.random() * 5
+	execAdbShellById(id, `input swipe ${postion.x} ${postion.y} ${endPosX} ${endPosY} 500`)
+}
